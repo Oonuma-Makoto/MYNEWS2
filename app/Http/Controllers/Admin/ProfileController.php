c6de2c23ffadcc640d4869da2bca73ce28320414
Oonuma-Makoto <renrino0210@gmail.com>
2021-12-20 15:05:49 +0900

    local17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
 
 use App\Profile;
 
 class ProfileController extends Controller
 {
-    //
     public function add()
  {
          return view('admin.profile.create');
@@ -21,6 +20,17 @@ class ProfileController extends Controller
     $profile = new Profile;
     $form = $request->all();
 
+      
+
+       // フォームから送信されてきた_tokenを削除する
+       unset($form['_token']);
+       // フォームから送信されてきたimageを削除する
+      
+
+        // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
 
  return redirect('admin/profile/create');
  }
@@ -28,9 +38,13 @@ class ProfileController extends Controller
 
 
 
-  public function edit()
+  public function edit(Request $request)
   {
-      return view('admin.profile.edit');
+    $profile = Profile::find($request->id);
+    if (empty($profile)) {
+      abort(404);    
+    }
+      return view('admin.profile.edit',['profile_form' => $profile]);
   }
 
   public function update()
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,13 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create'); 
-    /*課題3*/Route::post('profile/create','Admin\ProfileController@create');
-    /*課題6*/Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth'); 
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 Auth::routes();
 
